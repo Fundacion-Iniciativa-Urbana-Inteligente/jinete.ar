99e9f2e82c641733e561628c7be4bb3d7ea21da3
Gustavo Azzimonti Renzo <gazzimon@gmail.com>
2024-12-16 10:02:19 -0300

    Google Cloud Build

--- public/Pages/Home.js
+++
@@ -1,6 +0,0 @@
-import Mapa from './../Components/Mapa';
-export default function Home() {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
-    id: "mapa"
-  }, /*#__PURE__*/React.createElement(Mapa, null)));
-}
--- public/Pages/NotFound.js
+++
@@ -1,8 +0,0 @@
-import { NavLink } from "react-router-dom";
-export default function NotFound() {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
-    className: "container"
-  }, /*#__PURE__*/React.createElement("h1", null, "Not Found"), /*#__PURE__*/React.createElement("p", null, "Parece que te perdiste"), /*#__PURE__*/React.createElement("button", null, /*#__PURE__*/React.createElement(NavLink, {
-    to: "/"
-  }, "Volver al mapa"))));
-}
